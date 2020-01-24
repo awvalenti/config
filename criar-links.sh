4b0d20156849fef94882ce29d99c154da25f1e56
Andre Willik Valenti <andre.valenti@lambda3.com.br>
2020-01-24 19:55:04 -0300

    Atualizando

---
+++ criar-links.sh
@@ -0,0 +1,5 @@
+ln -s /home/andre/l3/proj/config/bash/.bash_aliases /home/andre/.bash_aliases
+
+ln -s /home/andre/l3/proj/config/vscode/keybindings.json /home/andre/.config/Code/User/keybindings.json
+ln -s /home/andre/l3/proj/config/vscode/settings.json /home/andre/.config/Code/User/settings.json
+
